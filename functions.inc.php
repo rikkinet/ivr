86353495f164de1105cc632183753c0b222f6c88
Matto9781 <74162167+Matto9781@users.noreply.github.com>
2023-02-15 22:46:55 +0000

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -136,6 +136,7 @@ function ivr_get_config($engine) {
 								$ext->add($c, 's', '', new ext_gotoif('$["${NODEFOUND}" = "0"]','beforewhile:nodedial'));
 							}
 						}else {
+							$ext->add($c, 's', '', new ext_playback('calling'));
 							$ext->add($c, 's', '', new ext_gotoif('$["${DIALPLAN_EXISTS(${CONTEXT},${DIGITS}${IVREXT},1)}" = "0"]','beforewhile:nodedial'));
 						}
 										$ext->add($c, 's', '', new ext_endwhile(''));
